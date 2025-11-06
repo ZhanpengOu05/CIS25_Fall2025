0c8e593cd88e24fa4594cd6d49847770f8e352fc
ZhanpengOu05 <30028125@cc.peralta.edu>
2025-11-06 03:02:31 -0800

    Refactor main.cpp to add user interaction

--- main.cpp
+++ main.cpp
@@ -1,17 +1,45 @@
 #include <iostream>
+#include <string>
 #include "BankAccount.h"
 using namespace std;
 
 int main() {
     BankAccount myAccount;
+    int choice = 0;
+    string item;
+    double amount;
 
-    myAccount.deposit(500.00);
-    myAccount.makePurchase("Coffee", 5.00);
-    myAccount.makePurchase("Notebook", 10.00);
-    myAccount.makePurchase("Headphones", 100.00);
+    cout << "Welcome to the Bank Account App!\n";
 
-    myAccount.displayBalance();
+    do {
+        cout << "\n1) Deposit\n2) Make Purchase\n3) Show Balance\n4) Exit\nChoice: ";
+        cin >> choice;
 
-    cout << "Transactions have been saved to transactions.txt\n";
+        switch (choice) {
+        case 1:
+            cout << "Enter amount to deposit: ";
+            cin >> amount;
+            myAccount.deposit(amount);
+            break;
+        case 2:
+            cout << "Enter item name: ";
+            cin >> item;
+            cout << "Enter cost: ";
+            cin >> amount;
+            myAccount.makePurchase(item, amount);
+            break;
+        case 3:
+            myAccount.displayBalance();
+            break;
+        case 4:
+            cout << "Saving transactions...\n";
+            myAccount.saveTransaction("End Session", 0);
+            break;
+        default:
+            cout << "Invalid choice.\n";
+        }
+    } while (choice != 4);
+
+    cout << "Goodbye!\n";
     return 0;
 }
